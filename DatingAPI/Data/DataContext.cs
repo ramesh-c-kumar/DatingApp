7f94d88187062d1b2884ba2883479b17dcc97b02
ramesh kumar <rameshkumar20294@gmail.com>
2019-09-22 11:14:04 +0530

    Initial Commit

---
+++ DatingAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DatingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPI.Data
+{
+    public class DataContext:DbContext
+    {
+       public DataContext(DbContextOptions<DataContext> options ): base(options) { } 
+       public DbSet<Value> Values {get;set;}
+    }
+}
